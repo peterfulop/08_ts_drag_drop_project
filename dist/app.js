1c815aa3698d3cd2745c220351e85f3572ca295b
peterfulop <mrpeterfulop@gmail.com>
2022-03-04 18:39:04 +0100

    feat(*):Render project input form as template

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,17 @@
 "use strict";
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const a = new ProjectInput();
+console.log(a.templateElement);
+console.log(a.hostElement);
+console.log(a.element);
