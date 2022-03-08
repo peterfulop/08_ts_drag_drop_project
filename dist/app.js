5eb99bb642560f5df85a4e5e1cafe47c916c7faa
peterfulop <mrpeterfulop@gmail.com>
2022-03-08 11:58:18 +0100

    func(*):Adding TypeScript Support with the ts-loader Package

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-import ProjectInput from "./components/project-input";
-import ProjectList from "./components/project-list";
-import { ProjectStatus } from "./models/project";
-new ProjectInput();
-new ProjectList(ProjectStatus.ACTIVE);
-new ProjectList(ProjectStatus.FINISHED);
