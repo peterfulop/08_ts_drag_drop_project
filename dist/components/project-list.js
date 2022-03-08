c572328b1fcd1ddf189ba88a9aa78f7449ddb6f2
peterfulop <mrpeterfulop@gmail.com>
2022-03-08 11:36:11 +0100

    func(*):Set webpack to bind TS files

--- dist/components/project-list.js
+++ dist/components/project-list.js
@@ -4,11 +4,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { Autobind } from "../decorators/autobind.js";
-import { ProjectStatus } from "../models/project.js";
-import { projectState } from "../state/project-state.js";
-import Component from "./base-component.js";
-import ProjectItem from "./project-item.js";
+import { Autobind } from "../decorators/autobind";
+import { ProjectStatus } from "../models/project";
+import { projectState } from "../state/project-state";
+import Component from "./base-component";
+import ProjectItem from "./project-item";
 export default class ProjectList extends Component {
     constructor(type) {
         super("project-list", "app", false, `${type}-projects`);
--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { Autobind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { ValidatableInput } from "../util/validation.js";
-import Component from "./base-component.js";
+import { Autobind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { ValidatableInput } from "../util/validation";
+import Component from "./base-component";
 
 export default class ProjectInput extends Component<
   HTMLDivElement,
